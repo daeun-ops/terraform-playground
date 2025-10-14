f913f349da3b22f639575cc38e16b11c71cfcd0e
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-14 10:26:29 +0900

    Create variables.tf

---
+++ eks-irsa-demo/modules/irsa/variables.tf
@@ -0,0 +1,6 @@
+variable "service"      { type = string }
+variable "sa_name"      { type = string }
+variable "namespace"    { type = string }
+variable "policy_tpl"   { type = string }
+variable "oidc_url"     { type = string }
+variable "oidc_arn"     { type = string }
