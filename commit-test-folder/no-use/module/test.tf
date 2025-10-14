94a272cd1f83facda405cd3a99c2e441719caff8
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-14 10:51:36 +0900

    Create test.tf

---
+++ commit-test-folder/no-use/module/test.tf
@@ -0,0 +1,5 @@
+irsa_role_arns = {
+  "cart"    = "arn:aws:iam::123456789012:role/irsa-role-cart"
+  "order"   = "arn:aws:iam::123456789012:role/irsa-role-order"
+  "payment" = "arn:aws:iam::123456789012:role/irsa-role-payment"
+}
